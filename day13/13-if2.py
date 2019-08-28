e871b211edf0f5d2650bb5cef9e06cda915750d2
esther Kim <esther4185@gmail.com>
2019-08-28 22:09:38 +0900

    if2

---
+++ day13/13-if2.py
@@ -0,0 +1,17 @@
+value=input("숫자값을 입력하세요.")
+
+inValue=int(value)
+
+if inValue >=50:
+    print("당신이 입력한 값은 50 이상 입니다.")
+else:
+    print("당신이 입력한 값은 50 미만 입니다.")
+
+x=input("숫자값을 입력하세요.")
+
+y=int(x)
+
+if y%2==0:
+    print("당신이 입력한 값은 짝수입니다.")
+else:
+    print("당신이 입력한 값은 홀수입니다.")
