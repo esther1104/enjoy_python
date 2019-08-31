af34c86a2c5f01809a288f6efb5c805f2522ac91
esther Kim <esther4185@gmail.com>
2019-08-31 21:12:37 +0900

    while2

--- day15/15-while.py
+++ day15/15-while.py
@@ -5,5 +5,3 @@ while(i<=100):
     sum=sum+i
 
 print(sum)
-
-print("무한 루트라 아무 것도 안 뜬다.")
---
+++ day16/16-while2.py
@@ -0,0 +1,22 @@
+x=1
+
+while x <=100:
+    if(x%2==0):
+        print(x)
+
+    x=x+1
+
+print("end")
+
+print("-----Homework-----")
+
+i=1
+
+total=0
+
+while i <=100:
+    if(i%2==1):
+        total=total+i
+    i=i+1
+
+print(total)
