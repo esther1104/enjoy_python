b0685ef2048ec0205ce8071cbfb36d21734d3e82
esther Kim <esther4185@gmail.com>
2019-09-04 22:17:26 +0900

    print and homework

--- day19/19-Homework.py
+++ day19/19-Homework.py
@@ -1,7 +1,9 @@
-X = 0
 
-while X < 10:
-    X = X + 1
-    if X == 5:
+x=1
+
+while x <= 10:
+    if x==5:
+        x=x+1
         continue
-    print(X)
+    print(x)
+    x=x+1
---
+++ day20/20-print.py
@@ -0,0 +1,15 @@
+print("hello", 100, 10.5, sep="\n")
+
+print("-----------------")
+
+print("hello", 100, 10.5, sep="-")
+
+print("------------------")
+
+print("hello", 100, 10.5, sep="-", end="-")
+
+print(sep="\n")
+
+print("-----heomework-----")
+for x in range(1,11):
+    print("*", end="")
