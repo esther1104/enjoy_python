bf9471e512b05fa83ae983ebdc42cfaec892ba99
esther Kim <esther4185@gmail.com>
2019-09-06 22:52:17 +0900

    hhhhhh

---
+++ day22/22-homework.py
@@ -0,0 +1,7 @@
+for x in range(1,6):
+    for y in range(0,x):
+        y=y+1
+        print(y,end="")
+    print()
+print()
+        
---
+++ day22/22-range.py
@@ -0,0 +1,5 @@
+for y in range(1,6):
+    for x in range(0,y):
+        print("*", end="")
+    print()
+print()
